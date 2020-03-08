65258223008a89eb6b32275af2340855f0580d48
B Legit <d.gc.fung@gmail.com>
2020-03-08 14:35:49 -0400

    react-app created and hamburger menu added

---
+++ blog/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+      <React.Fragment>
+
+        {/* render routes here */}
+        <Header/>
+      </React.Fragment>
+  );
+}
+
+export default App;
---
+++ blog/client/src/components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+
+class Header extends React.Component {
+  container = React.createRef(); 
+  state = {
+    open: false,
+  };
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+
+componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+}
+componentWillUnmount() {
+  document.removeEventListener("mousedown", this.handleClickOutside);
+}
+
+handleClickOutside = event => {
+  if (this.container.current && !this.container.current.contains(event.target)) {
+    this.setState({
+      open: false,
+    });
+  }
+};
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container" ref= {this.container}>
+        <h1>Blogger 2020</h1>
+          <button type="button" class="button" onClick= {this.handleButtonClick}>
+            ☰
+          </button>
+          {this.state.open && (
+          <div class="dropdown" onClick={this.handleButtonClick}>
+            <ul>
+              <li>Option 1</li>
+              <li>Option 2</li>
+              <li>Option 3</li>
+              <li>Option 4</li>
+            </ul>
+          </div>
+           )}
+        </div>
+       
+      </div>
+    );
+
+  }
+}
+
+export default Header
