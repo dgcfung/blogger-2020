91e9bc038c13965978ed10bbfafab8c2985e564f
B Legit <d.gc.fung@gmail.com>
2020-03-09 12:16:11 -0400

    SignIn form, not functioning

--- blog/client/src/components/SignIn.js
+++ blog/client/src/components/SignIn.js
@@ -6,7 +6,24 @@ class SignIn extends React.Component{
         return(
             <div>
             <h1>Sign In</h1> 
-            <hr />
+            {/* <hr />
+
+            <form onSubmit= {(e)=>{
+                e.preventDefault();
+                this.props.handleLogin();
+
+            }}>
+            <p>Email:</p>
+            <input name ="email" type= "text" value={this.props.formData.email} onChange={this.props.handleChange}/>
+            <p>Password:</p>
+            <input name="password" type= "password" value={this.props.FormData.password} onChange= {this.props.handleChange}/>
+            <hr/>
+            <button>Sign In</button>
+            <Link to="/login/signup">Sign Up</Link>
+            
+
+
+            </form> */}
             </div>
         )
 
