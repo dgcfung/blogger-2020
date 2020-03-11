feb4bf10a193c81342fbce168a756ce193e48465
B Legit <d.gc.fung@gmail.com>
2020-03-11 19:15:44 -0400

    3rd model addded, up to 6.4 tutorial, created controller

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   end
 
   get :all_posts, controller: "posts"
+
+  resources :posts do
+    resources :comments
+  end
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_165957) do
+ActiveRecord::Schema.define(version: 2020_03_11_222016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -35,5 +45,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_165957) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
